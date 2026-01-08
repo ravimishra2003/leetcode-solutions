46a0fca3890adc3b51f320cc10073a25ab022692
Ravi Prakash <103569866+ravimishra2003@users.noreply.github.com>
2026-01-09 00:08:01 +0530

    Time: 1 ms (99.76%) | Memory: 45.7 MB (14.41%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,26 +1,19 @@
 class Solution {
     public int compress(char[] chars) {
-        int idx = 0;  // write pointer
-        int i = 0;    // read pointer
-
-        while (i < chars.length) {
-            char current = chars[i];
-            int count = 0;
-
-            // count consecutive characters
-            while (i < chars.length && chars[i] == current) {
-                i++;
+        int i=0;
+        int idx=0;
+        while(i<chars.length){
+            int count=0;
+            char ch=chars[i];
+            while(i<chars.length && chars[i]==ch){
                 count++;
+                i++;
             }
-
-            // write character
-            chars[idx++] = current;
-
-            // write count if > 1
-            if (count > 1) {
-                char[] freq = Integer.toString(count).toCharArray();
-                for (char c : freq) {
-                    chars[idx++] = c;
+            chars[idx++]=ch;
+            if(count>1){
+                char freq[]=Integer.toString(count).toCharArray();
+                for(char c:freq){
+                    chars[idx++]=c;
                 }
             }
         }

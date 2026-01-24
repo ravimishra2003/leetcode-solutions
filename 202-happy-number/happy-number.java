ad943c573375a2d6ace310ec11e7d380b5d6920e
Ravi Prakash <103569866+ravimishra2003@users.noreply.github.com>
2026-01-25 02:02:39 +0530

    Time: 1 ms (76.45%) | Memory: 42.4 MB (56.72%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set=new HashSet<>();
+        while(true){
+            int sum=0;
+            while(n>0){
+                int rem=n%10;
+                sum=sum+rem*rem;
+                n=n/10;
+            }
+            if(sum==1){
+                return true;
+            }
+            else{
+                n=sum;
+            }
+            if(set.contains(n)){
+                return false;
+            }
+            else{
+                set.add(n);
+            }
+        }
+    }
+}
